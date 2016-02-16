cad95a93c2106d9c57110f324714d89b30aca8db
Micah Wells <micahp0506@gmail.com>
2016-02-16 09:34:19 -0600

    modualrizing code

--- routes/quote.js
+++ routes/quote.js
@@ -1,11 +1,11 @@
-'use strict';
+'use strict'
 
 
 const express = require('express');
 const router = express.Router();
 
-const quote = require('./quote');
+const quote = require('../controllers/quote');
 
-router.use(quote);
+router.use('/', quote.index);
 
 module.exports = router;
