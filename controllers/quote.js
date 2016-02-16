4371b068e25ddf767a461506a6ffbf26f30b6ade
Micah Wells <micahp0506@gmail.com>
2016-02-16 16:19:26 -0600

    Getting data from API

--- controllers/quote.js
+++ controllers/quote.js
@@ -11,13 +11,14 @@ module.exports.newQuote = (req) => {
     // Add 'res' back into the argument
     // console.log("info", req.body.symbol);
     let symbol = req.body.symbol;
-    const url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + `${symbol}`;
-    request.get(url, (err) => {
+    const url = `http://dev.markitondemand.com/Api/v2/Quote/json?symbol=${symbol}`;
+    request.get(url, (err, response, body) => {
         // Add 'body' $ 'response' back into argument
         if (err) throw err;
-        // console.log("Symbol", body.Symbol);
-        // console.log("Name", body.Name);
-        // console.log("LastPrice", body.LastPrice);
+        let result = JSON.parse(body);
+        console.log("Symbol", result.Symbol);
+        console.log("Name", result.Name);
+        console.log("LastPrice", result.LastPrice);
         // res.send(JSON.parse(body));
     });
 
