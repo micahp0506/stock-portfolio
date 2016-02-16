71b1f8a30e839e71df8db84bcd106c255da23f2f
Micah Wells <micahp0506@gmail.com>
2016-02-15 18:20:47 -0600

    Adding mongodb link

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
 const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
 const MONGODB_USER = process.env.MONGODB_USER || '';
 const MONGODB_PASS = process.env.MONGODB_PASS || '';
-const MONGODB_NAME = process.env.MONGODB_NAME || 'node-webserver';
+const MONGODB_NAME = process.env.MONGODB_NAME || 'stock-portfolio';
 const MONGODB_URL_PREFIX = MONGODB_USER ? `${MONGODB_USER}:${MONGODB_PASS}@` : '';
 
 const MONGODB_URL = `mongodb://${MONGODB_URL_PREFIX}${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_NAME}`;
